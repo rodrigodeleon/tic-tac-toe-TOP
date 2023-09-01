caaab9a79e1be8b63de368987fac586108fbd9c5
rodrigodeleon <20547168+rodrigodeleon@users.noreply.github.com>
2023-09-01 18:44:23 +0200

    Fix Turns and prints Players names

--- javascript.js
+++ javascript.js
@@ -30,39 +30,47 @@ const gameBoard = (function () {
 })();
 
 const Game = (function () {
+  const gameboard = gameBoard.newGameBoard();
+  const PlayerX = Player(prompt("Choose Player X Name"), "X");
+  const PlayerO = Player(prompt("Choose Player O Name"), "O");
+  let activePlayer = PlayerX;
+  let gameEnd = false;
+  console.table(players);
   const newGame = () => {
-    const PlayerX = Player(prompt("Choose Player X Name"), "X");
-    const PlayerO = Player(prompt("Choose Player O Name"), "O");
-    const players = [];
-    players.push(PlayerX, PlayerO);
-    const activePlayer = PlayerX;
-    const gameEnd = false;
-    console.table(players);
-    const gameboard = gameBoard.newGameBoard();
     displayController.showGameBoard(gameboard);
-    while (!gameEnd)
-    {
-        newPlay()
-    }
-  };
-  function newPlay(cellId) {
+    // while (!gameEnd) {
 
+    // }
+    // alert('game finished!');
+  };
+  function newPlay(cellChosen) {
+    const myobj = gameboard.find((element) => element.id == cellChosen);
+    myobj.available = false;
+    myobj.playedBy = activePlayer;
+    console.log(myobj);
+    activePlayer == PlayerX
+      ? (activePlayer = PlayerO)
+      : (activePlayer = PlayerX);
+    displayController.showGameBoard(gameboard);
   }
 
-  return { newGame };
+  return { newGame, newPlay };
 })();
 
 const displayController = (function () {
   const gameBoardDisplay = document.querySelector("#display");
   const showGameBoard = function (gameboard) {
+    gameBoardDisplay.innerHTML = "";
     gameboard.forEach((element) => {
       let cell = document.createElement("div");
       cell.classList.add("cell");
       cell.setAttribute("id", element.id);
       element.available
         ? (cell.style.backgroundColor = "light-blue")
-        : (cell.innerHTML = element.playedBy);
+        : (cell.innerHTML = element.playedBy.name);
       gameBoardDisplay.appendChild(cell);
+
+      cell.addEventListener("click", () => Game.newPlay(cell.id));
     });
   };
 
