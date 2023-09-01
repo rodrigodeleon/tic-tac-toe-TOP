0c673c77ee9ed23b3952e69bd9e7172028f7bc4d
rodrigodeleon <20547168+rodrigodeleon@users.noreply.github.com>
2023-09-01 18:02:06 +0200

    Start Game Logic

--- javascript.js
+++ javascript.js
@@ -1,12 +1,20 @@
-const Player = (name, nationality) => {
-  return { name, nationality };
+const Player = (name, id) => {
+  const game = [];
+  function regPlayedCell(cellId) {
+    game.push(cellId);
+  }
+  return { name, id, regPlayedCell };
 };
 
 const gameBoard = (function () {
   let gameboard = [];
   const newGameBoard = () => {
     for (let index = 1; index < 10; index++) {
-      gameboard.push(index);
+      gameboard.push({
+        id: index,
+        available: true,
+        playedBy: "",
+      });
     }
     return gameboard;
   };
@@ -22,33 +30,43 @@ const gameBoard = (function () {
 })();
 
 const Game = (function () {
-  const PlayerA = Player("Rodrigo", "Uruguay");
-  const PlayerB = Player("Esther", "France");
   const newGame = () => {
-    const newGameboard = gameBoard.newGameBoard();
-    displayController.showGameBoard(newGameboard);
+    const PlayerX = Player(prompt("Choose Player X Name"), "X");
+    const PlayerO = Player(prompt("Choose Player O Name"), "O");
+    const players = [];
+    players.push(PlayerX, PlayerO);
+    const activePlayer = PlayerX;
+    const gameEnd = false;
+    console.table(players);
+    const gameboard = gameBoard.newGameBoard();
+    displayController.showGameBoard(gameboard);
+    while (!gameEnd)
+    {
+        newPlay()
+    }
   };
+  function newPlay(cellId) {
+
+  }
+
+  return { newGame };
 })();
 
 const displayController = (function () {
   const gameBoardDisplay = document.querySelector("#display");
-
   const showGameBoard = function (gameboard) {
     gameboard.forEach((element) => {
       let cell = document.createElement("div");
       cell.classList.add("cell");
-      cell.setAttribute('id', element);
-      cell.innerHTML = element;
+      cell.setAttribute("id", element.id);
+      element.available
+        ? (cell.style.backgroundColor = "light-blue")
+        : (cell.innerHTML = element.playedBy);
       gameBoardDisplay.appendChild(cell);
     });
-
-    // cell.addEventListener("click", () => {
-
-    // });
   };
 
   return { showGameBoard };
 })();
 
-const newGameboard = gameBoard.newGameBoard();
-displayController.showGameBoard(newGameboard);
+Game.newGame();
