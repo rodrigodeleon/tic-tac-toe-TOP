919aa8589a5085cda01a974ae54d250ad256a9ef
rodrigodeleon <20547168+rodrigodeleon@users.noreply.github.com>
2023-09-01 20:48:01 +0200

    Finish game logic and all basics

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 const Player = (name, id) => {
   const game = [];
+  const getGame = () => {
+    return game;
+  };
   function regPlayedCell(cellId) {
-    game.push(cellId);
+    game.push(parseInt(cellId));
   }
-  return { name, id, regPlayedCell };
+  return { name, id, regPlayedCell, getGame };
 };
 
 const gameBoard = (function () {
@@ -34,31 +37,67 @@ const Game = (function () {
   const PlayerX = Player(prompt("Choose Player X Name"), "X");
   const PlayerO = Player(prompt("Choose Player O Name"), "O");
   let activePlayer = PlayerX;
-  let gameEnd = false;
-  console.table(players);
+  let result;
   const newGame = () => {
     displayController.showGameBoard(gameboard);
-    // while (!gameEnd) {
-
-    // }
-    // alert('game finished!');
+    displayController.showPlayers(PlayerX, PlayerO);
   };
   function newPlay(cellChosen) {
     const myobj = gameboard.find((element) => element.id == cellChosen);
     myobj.available = false;
     myobj.playedBy = activePlayer;
-    console.log(myobj);
+    activePlayer.regPlayedCell(cellChosen);
+    console.log(activePlayer.getGame());
+    result = checkResults(activePlayer.getGame());
+    console.log(result);
+    if (result) {
+      alert(`End of the Game, ${activePlayer.name} Wins!!`);
+    }
+
     activePlayer == PlayerX
       ? (activePlayer = PlayerO)
       : (activePlayer = PlayerX);
     displayController.showGameBoard(gameboard);
   }
 
+  function checkResults(playerGame) {
+    let result = false;
+    if (playerGame.length > 2) {
+      const winningCombos = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+      ];
+
+      winningCombos.forEach((combo) => {
+        if (checkCombo(combo, playerGame)) {
+          result = true;
+        }
+      });
+
+      function checkCombo(winningCombo, playerGame) {
+        return winningCombo.every((value) => {
+          return playerGame.includes(value);
+        });
+      }
+    }
+    return result;
+  }
+
   return { newGame, newPlay };
 })();
 
 const displayController = (function () {
-  const gameBoardDisplay = document.querySelector("#display");
+  const gameBoardDisplay = document.querySelector("#gameboardDisplay");
+  const playersDisplay = document.querySelector("#playersDisplay");
+  const showPlayers = function (playerX, playerO) {
+    playersDisplay.innerHTML = `${playerX.name} Plays with X and ${playerO.name} Plays with O`;
+  };
   const showGameBoard = function (gameboard) {
     gameBoardDisplay.innerHTML = "";
     gameboard.forEach((element) => {
@@ -67,14 +106,14 @@ const displayController = (function () {
       cell.setAttribute("id", element.id);
       element.available
         ? (cell.style.backgroundColor = "light-blue")
-        : (cell.innerHTML = element.playedBy.name);
+        : (cell.innerHTML = element.playedBy.id);
       gameBoardDisplay.appendChild(cell);
 
       cell.addEventListener("click", () => Game.newPlay(cell.id));
     });
   };
 
-  return { showGameBoard };
+  return { showGameBoard, showPlayers };
 })();
 
 Game.newGame();
